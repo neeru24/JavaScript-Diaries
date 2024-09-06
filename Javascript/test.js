b4b26939bea851eddf4413ce9ccab381330d5f03
Neeru <161798182+neeru24@users.noreply.github.com>
2024-09-06 13:06:41 +0000

    Changes done

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,1 +1,22 @@
-console.log("Neeru");
+const accountId = 1553
+let accountEmail = "neeru@google.com"
+var accountPassword = "12345"
+accountCity = "Ludhiana"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "ng@ng.com"
+accountPassword = "21212121"
+accountCity = "LDH"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
