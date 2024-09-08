014e6773b79aac18287cee73448bbfea0aebb477
Neeru <161798182+neeru24@users.noreply.github.com>
2024-09-08 14:37:57 +0000

    Commit Changes

---
+++ Javascript/comparison.js
@@ -0,0 +1,32 @@
+console.log("Simple comparisons ");
+
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// Different datatypes
+
+console.log("Different Datatypes ");
+console.log("2" > 1);
+console.log("02" > 1);
+
+// Null
+
+console.log("Null : ");
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// Undefined 
+
+console.log("Undefined: ");
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === Strict Equality 
+
+console.log("Strict Check");
+console.log("2" === 2);
