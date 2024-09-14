52f3a2714252305ae955d7eb945d618563089e23
Neeru <161798182+neeru24@users.noreply.github.com>
2024-09-14 10:58:29 +0000

    Commit changes

--- Javascript/dateTime.js
+++ Javascript/dateTime.js
@@ -1,31 +1,76 @@
-// Dates
+let currentDate = new Date();
+console.log("Current Date:", currentDate);
 
-let myDate = new Date()
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(typeof myDate);
+let specificDate = new Date(2024, 8, 14, 10, 30, 0);
+console.log("Specific Date:", specificDate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+let timeStamp = Date.parse("September 14, 2024 10:30:00");
+console.log("Timestamp:", timeStamp);
 
-let myTimeStamp = Date.now()
+let year = currentDate.getFullYear();
+console.log("Year:", year);
 
-console.log(myTimeStamp);
-console.log(myCreatedDate.getTime());
-console.log(Math.floor(Date.now()/1000));
+let month = currentDate.getMonth();
+console.log("Month:", month);
 
-let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+let day = currentDate.getDate();
+console.log("Day of the month:", day);
 
-// `${newDate.getDay()} and the time `
+let weekDay = currentDate.getDay();
+console.log("Day of the week:", weekDay);
 
-newDate.toLocaleString('default', {
-    weekday: "long",
-    
-})
+let hours = currentDate.getHours();
+console.log("Hours:", hours);
+
+let minutes = currentDate.getMinutes();
+console.log("Minutes:", minutes);
+
+let seconds = currentDate.getSeconds();
+console.log("Seconds:", seconds);
+
+let milliseconds = currentDate.getMilliseconds();
+console.log("Milliseconds:", milliseconds);
+
+let time = currentDate.getTime();
+console.log("Time (Timestamp):", time);
+
+let utcHours = currentDate.getUTCHours();
+console.log("UTC Hours:", utcHours);
+
+let newDate = new Date();
+newDate.setFullYear(2025);
+console.log("Set Year to 2025:", newDate);
+
+newDate.setMonth(0);
+console.log("Set Month to January:", newDate);
+
+newDate.setDate(1);
+console.log("Set Day of the month to 1:", newDate);
+
+newDate.setHours(8);
+console.log("Set Hours to 8:", newDate);
+
+newDate.setMinutes(45);
+console.log("Set Minutes to 45:", newDate);
+
+newDate.setSeconds(15);
+console.log("Set Seconds to 15:", newDate);
+
+newDate.setMilliseconds(300);
+console.log("Set Milliseconds to 300:", newDate);
+
+newDate.setTime(1726350600000);
+console.log("Set Time using timestamp:", newDate);
+
+console.log("toString():", newDate.toString());
+console.log("toDateString():", newDate.toDateString());
+console.log("toTimeString():", newDate.toTimeString());
+console.log("toLocaleDateString():", newDate.toLocaleDateString());
+console.log("toLocaleTimeString():", newDate.toLocaleTimeString());
+console.log("toISOString():", newDate.toISOString());
+console.log("toUTCString():", newDate.toUTCString());
+
+let nowTimestamp = Date.now();
+console.log("Current Timestamp (Date.now()):", nowTimestamp);
+
+console.log("valueOf():", newDate.valueOf());
