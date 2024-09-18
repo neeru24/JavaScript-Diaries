f77cf26430410cb7c3866721d45e8ba6a1a051d2
Neeru <161798182+neeru24@users.noreply.github.com>
2024-09-18 13:50:32 +0000

    Commit Changes

---
+++ Array-Objects/04_Object.js
@@ -0,0 +1,61 @@
+// const newUser = new Object()
+const newUser = {}
+
+newUser.id = "123abc"
+newUser.name = "neeru"
+newUser.isLoggedIn = false
+
+// console.log(newUser);
+
+const regularUser = {
+    email: "neeru@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "neeru",
+            lastname: "gangarh"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+const obj3 = { obj1, obj2 }
+const obj5 = Object.assign({}, obj1, obj2, obj4)
+
+const obj6 = {...obj1, ...obj2}
+console.log(obj6);
+
+
+
+const users = [
+    {
+        id: 1,
+        email: "ng@gmail.com"
+    },
+    {
+        id: 1,
+        email: "ng@gmail.com"
+    },
+    {
+        id: 1,
+        email: "ng@gmail.com"
+    },
+]
+
+
+users[1].email
+console.log(newUser);
+
+
+console.log(Object.keys(newUser));
+console.log(Object.values(newUser));
+console.log(Object.entries(newUser));
+
+
+
+console.log(newUser.hasOwnProperty('isLoggedIn'));
+
